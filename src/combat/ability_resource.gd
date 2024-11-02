2f764cd79f8bb890fff663e5b2cad5724f9a6174
kno3comma14 <enyert.vinas@gmail.com>
2024-11-02 08:06:58 -0500

    Working on game logic and fixed visibilty of the board

--- src/combat/ability_resource.gd
+++ src/combat/ability_resource.gd
@@ -3,7 +3,8 @@ class_name AbilityResource
 
 enum AbilityType {
 	DAMAGE,
-	HEALING
+	HEALING,
+	SUPPORT
 }
 
 
@@ -11,4 +12,9 @@ enum AbilityType {
 @export_multiline var description: String
 @export var hp_effect_intensity: int
 @export var energy_effect_intensity: int
+@export var hp_comsuption: int
+@export var energy_comsuption: int
 @export var cooldown: int
+@export var duration: int
+@export var tick_period: int
+@export var type: AbilityType
